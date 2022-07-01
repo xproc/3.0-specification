4fb066aa1a3eba486bc18642777ac45acb33bb4f
Norman Walsh <ndw@nwalsh.com>
2022-07-01 07:44:24 +0100

    Make diff against correct last call draft

--- tools/difftools.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$DIFFPASSPHRASE" != "" ]; then
-    openssl aes-256-cbc -d -k $DIFFPASSPHRASE -in tools/difftools.enc -out diff.tar.gz
-    tar zxf diff.tar.gz
-    rm -f diff.tar.gz
-fi
